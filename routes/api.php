31f6022aa792e587afcb1c80daa12676e3cbabb9
ArisTech <aristechdev@gmail.com>
2025-06-18 09:56:11 +0000

    Update api.php
    API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ValidationController;
+use App\Http\Controllers\API\ValidationController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
